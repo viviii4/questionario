6310f14db6abc47253b16c64e3e5dc00c265c4a7
viviii4 <edilzadacosta58@gmail.com>
2024-05-26 01:10:49 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,49 @@
+export default [
+  {
+    question: "O que é hemofilia?",
+    answers: [
+      { option: "Uma doença que afeta a coagulação do sangue", correct: true },
+      { option: "Uma doença que afeta o sistema imunológico", correct: false },
+      { option: "Uma infecção causada por bactérias", correct: false },
+    ],
+  },
+  {
+    question: "Qual tipo de hemofilia é mais comum?",
+    answers: [
+      { option: "Hemofilia A", correct: true },
+      { option: "Hemofilia B", correct: false },
+      { option: "Hemofilia C", correct: false },
+    ],
+  },
+  {
+    question: "A hemofilia é uma doença hereditária. Verdadeiro ou Falso?",
+    answers: [
+      { option: "Verdadeiro", correct: true },
+      { option: "Falso", correct: false },
+    ],
+  },
+  {
+    question: "Qual é o tratamento mais comum para a hemofilia?",
+    answers: [
+      { option: "Transplante de medula óssea", correct: false },
+      { option: "Reposição do fator de coagulação", correct: true },
+      { option: "Antibióticos", correct: false },
+    ],
+  },
+  {
+    question: "Quais são os principais sintomas da hemofilia?",
+    answers: [
+      { option: "Sangramentos prolongados e hematomas fáceis", correct: true },
+      { option: "Febre alta e dor de cabeça", correct: false },
+      { option: "Cansaço e perda de apetite", correct: false },
+    ],
+  },
+  {
+    question: "Qual gene está mutado na hemofilia A?",
+    answers: [
+      { option: "Gene F8", correct: true },
+      { option: "Gene F9", correct: false },
+      { option: "Gene HBB", correct: false },
+    ],
+  },
+];
